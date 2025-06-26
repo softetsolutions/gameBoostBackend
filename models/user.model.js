79c1c892f0e4c952aac764c22b14dba1fd113207
SANDEEP25 <sandeep.yd951@gmail.com>
2025-06-26 22:40:27 +0530

    register api modification

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName:  { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'seller'], default: 'user' },
