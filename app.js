fab76d271b743cff5903e9900543ff80cea56cea
SANDEEP25 <sandeep.yd951@gmail.com>
2025-06-29 13:11:55 +0530

    payload too large error fix

--- app.js
+++ app.js
@@ -27,8 +27,12 @@ const io = new Server(server);
 
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Fix for 413 Payload Too Large
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+
 app.use(compression());
-app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
